8a66373c577fa21466b385699c6e8d8596858085
mohamadjf900 <126292710+mohamadjf900@users.noreply.github.com>
2026-02-16 12:50:06 +0330

    Update buy.py

--- handlers/buy.py
+++ handlers/buy.py
@@ -11,7 +11,7 @@ async def buy_menu(callback: CallbackQuery):
     await db.log_usage(callback.from_user.id, "buy")
     text = (
         "💼 برای خرید کانفیگ اختصاصی یا VPN با کیفیت، با ادمین در ارتباط باشید:\n\n"
-        f"🆔 @Mj054 (یوزرنیم ادمین را اینجا قرار دهید)\n\n"
+        f"🆔 @Mj054 \n\n"
         "یا از طریق ربات پشتیبانی اقدام کنید."
     )
     await callback.message.edit_text(text, reply_markup=back_button())
