ded228245c2d096d91ecfdfe12aeb6b5af89081e
mohamadjf900 <126292710+mohamadjf900@users.noreply.github.com>
2026-02-19 17:38:02 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from config import BOT_TOKEN
-from handlers import start, proxy, v2ray, wireguard, dns, buy, support, admin, ping, ticket  # ticket اضافه شد
+from handlers import start, proxy, v2ray, wireguard, dns, buy, support, admin, ticket  # ping حذف شد
 from middlewares import CheckSubscriptionMiddleware
 from database import init_db
 import keep_alive
@@ -37,8 +37,7 @@ async def main():
     dp.include_router(buy.router)
     dp.include_router(support.router)
     dp.include_router(admin.router)
-    dp.include_router(ping.router)
-    dp.include_router(ticket.router)  # روت جدید
+    dp.include_router(ticket.router)  # روت تیکت
 
     await set_commands(bot)
     await dp.start_polling(bot)
