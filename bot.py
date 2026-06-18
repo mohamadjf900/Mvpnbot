6b2d5448e1fb85af0acf30fc6252beee8251f2a9
mohamadjf900 <126292710+mohamadjf900@users.noreply.github.com>
2026-06-19 02:12:49 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,31 +3,12 @@ import telebot
 from telebot import types
 import json
 import sqlite3
-from threading import Thread
-from flask import Flask
 from config import BOT_TOKEN, CHANNEL_ID
 from database import *
 from middlewares import *
 
 # اتصال به توکن ربات از طریق متغیرهای محیطی رندر
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN', BOT_TOKEN))
-
-# --- بخش فیک سرور وب برای زنده نگه داشتن رندر ---
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running 24/7!"
-
-def run_web_server():
-    # رندر خودش پورت را تعیین می‌کند، اگر نبود روی پورت 10000 می‌رود
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
-
-def keep_alive():
-    t = Thread(target=run_web_server)
-    t.start()
-# ----------------------------------------
 
 # تلاش برای ساخت دیتابیس
 try:
@@ -48,8 +29,5 @@ except Exception as e:
     print(f"Note on handlers: {e}")
 
 if __name__ == "__main__":
-    print("--- Starting Web Server for Render ---")
-    keep_alive() # روشن کردن سرور وب در پس‌زمینه
-    
     print("--- Robot Started Successfully on Render! ---")
     bot.infinity_polling(timeout=60, long_polling_timeout=60)
